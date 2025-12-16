8f291a04ff332963cb9734cf321e561665a4dc9b
shalabhraj1990 <shalabhrajetc@gmail.com>
2025-12-16 07:32:31 +0530

    converted swiggy-mcp to streamamble http

--- swiggy-mcp/customercare.py
+++ swiggy-mcp/customercare.py
@@ -113,5 +113,6 @@ def read_markdown_file(file_path):
         return None
     
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+    #mcp.run(transport="stdio")
+    mcp.run(transport="streamable-http")
         
